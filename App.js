391e40532d59cc32aa84de0ad0a16bcfa54f2fae
Rachna Yadav <rachnaunboxindustry@gmail.com>
2023-09-24 14:07:02 +0530

    igniting the app

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-{/* <div id="parent">
-    <div id="child">
-        <h1>I am h1 tag</h1>
-    </div>
-</div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
 
 
 const parent=React.createElement("div" , {id:"parent"},[
 React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"hey I am jayant kumar")]
 ),
 React.createElement("div",{id:"child"},
 [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
