a3683a5d6c8a3325ed89756e273ef1ec0ff7429f
Rachna Yadav <rachnaunboxindustry@gmail.com>
2023-10-12 12:05:38 +0530

    react element and component

--- App.js
+++ App.js
@@ -1,26 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// const parent=React.createElement("div" , {id:"parent"},[
+// React.createElement("div",{id:"child"},
+// [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"hey I am jayant kumar")]
+// ),
+// React.createElement("div",{id:"child"},
+// [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+// )]
+// // );
+// console.log(parent)
 
 
+//React Element
+const jsxHeading = 
+<h1 id="heading">Hey I am Rachna Jayant Yadav</h1>;
+// console.log(jsxHeading)
 
+//React functional component
 
+const HeadingComponent=() =>{
+    return <h1>Namaste React Functional component</h1>
+};
 
-
-const parent=React.createElement("div" , {id:"parent"},[
-React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"hey I am jayant kumar")]
-),
-React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-)]
+const HeadingComponent2=()=>(
+    <div>
+        {/* "<HeadingComponent/>" */}
+        {/* <h1>{console.log("rachna")}</h1> */}
+        {jsxHeading}
+        {jsxHeading}
+        {jsxHeading}
+        {10000}
+        {HeadingComponent()}
+        <HeadingComponent/>
+        <h1>
+        Namaste React Functional component
+        </h1>
+    </div>
 );
 
-
-// const heading = React.createElement
-// ("h1",
-// {id:"heading", xyz:"abc"},
-// "helloWorld from react");
-console.log(parent)
-
-const root= ReactDOM.createRoot(document.getElementById("header"));
-root.render(parent);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+root.render(<HeadingComponent2/>)
