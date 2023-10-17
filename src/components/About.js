6660ad4047de5c94427b9eeace8aa3b0500194b5
Rachna Yadav <rachna.tjr@gmail.com>
2023-10-17 12:51:57 +0530

    routing the pages

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const About=()=>{
+    return(
+        <div>
+            <p>Swiggy Labs is 
+                a self-contained extension of Swiggy that drives experiments and 0 to 1s for potential new business lines. 
+                This inturn helps us contribute to shaping the future of how Swiggy will continue to provide unparalleled convenience 
+                to its customers.</p>
+        </div>
+
+    )
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+const Contact=()=>{
+    return(
+        <div>
+            <p>The essential elements of an 
+                excellent Contact Us page include a clear call to action, easy navigation, and a message that resonates with </p>
+        </div>
+    )
+
+    
+}
+export default Contact;
