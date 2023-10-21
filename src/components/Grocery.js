6f6510d29f2f4664b2ebdb58dcf99feefa45dc4d
Rachna Yadav <rachna.tjr@gmail.com>
2023-10-21 13:43:20 +0530

    online offlin status and optimizing the app using lazy loading and own customize hooks

---
+++ src/components/Grocery.js
@@ -0,0 +1,9 @@
+const Grocery =()=>{
+    return (
+        <h1>
+            {""}
+            Our Grocery online store,we have alot child component in this web page.
+        </h1>
+    )
+}
+export default Grocery;
